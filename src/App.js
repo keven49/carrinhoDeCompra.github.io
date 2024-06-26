67d11aa5a7259e4bec4869c41f7f89bc093d9a95
keven49 <109768754+keven49@users.noreply.github.com>
2024-06-26 15:54:28 -0300

    Update App.js
    atualização da página inicial.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { CartProvider } from './store/AppContext'; 
 import { HeaderPartial } from './headerPartial/headerPartial';
 import { ProductListContainer } from './containenrs/ProductListContainer/ProductListContainer';
@@ -8,18 +8,16 @@ import { CartContainer } from './containenrs/CartContainer/CartContainer';
 function App() {
   return (
     <CartProvider>
-      
         <BrowserRouter>
           <div className="App">
-            <HeaderPartial />
-            <Routes>
-              <Route path='/home' element={<ProductListContainer />} />
-              <Route path="/productList" element={<ProductListContainer />} />
-              <Route path="/cart" element={<CartContainer />} />
-            </Routes>
+          <HeaderPartial />
+              <Routes>
+                  <Route path="/" element={<Navigate to="/productList" />} />
+                  <Route path="/productList" element={<ProductListContainer />} />
+                  <Route path="/cart" element={<CartContainer />} />
+              </Routes>
           </div>
         </BrowserRouter>
-      
     </CartProvider>
   );
 }
